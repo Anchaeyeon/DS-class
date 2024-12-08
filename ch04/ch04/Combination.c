d793577ea0bff7ee1574e0f48e4a5a0aa9922287
Anchaeyeon <s2328@e-mirim.hs.kr>
2024-12-09 04:09:07 +0900

    조합

---
+++ ch04/ch04/Combination.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define R 3
+
+int b[R], cnt = 0;
+
+void printCom() {
+    for (int i = 0; i < R; i++) printf("%5d", b[i]);
+    printf("\n"); cnt++;
+}
+
+void Combination(int a[], int n, int r, int ii) {
+    int  i;
+    if (r == R) printCom();
+    else
+        for (i = ii + 1; i < n; i++) {
+            b[r] = a[i];
+            Combination(a, n, r + 1, i);
+        }
+}
+
+int main(void) {
+    int a[] = { 1, 2, 3, 4, 5 };
+    int n = sizeof(a) / sizeof(int);
+
+    Combination(a, n, 0, -1);
+
+    printf("조합 개수: %d\n", cnt);
+
+    return 0;
+}
